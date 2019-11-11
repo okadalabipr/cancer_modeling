0a62eede6baeb059d07d52d3ce555f8c12edca58
himoto <himoto@protein.osaka-u.ac.jp>
2019-11-11 12:00:48 +0900

    Add 'non-zero'

--- model_dev/model/initial_condition.py
+++ model_dev/model/initial_condition.py
@@ -3,7 +3,7 @@ from .name2idx import variables as V
 def initial_values():
     y0 = [0]*V.len_f_vars
     """
-    This is where you define initial concentrations.
+    This is where you define non-zero initial concentrations.
     """
 
     return y0

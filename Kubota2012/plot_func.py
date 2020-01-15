9593747d9048cf335d1c77846e35c4c161bb8298
himoto <himoto@protein.osaka-u.ac.jp>
2020-01-15 09:45:22 +0900

    fontweight='bold'

--- Kubota2012/plot_func.py
+++ Kubota2012/plot_func.py
@@ -8,6 +8,7 @@ def timecourse(sim):
     plt.rcParams['font.size'] = 18
     plt.rcParams['lines.linewidth'] = 4
     plt.rcParams['lines.markersize'] = 16
+    
     plt.subplots_adjust(wspace=0.25, hspace=0.3)
 
     plt.subplot(2, 2, 1)
@@ -17,11 +18,11 @@ def timecourse(sim):
     plt.plot(sim.t, sim.pAKT[:, 3], 'goldenrod')
     plt.plot(sim.t, sim.pAKT[:, 4], 'brown')
     plt.xlim(0, 480)
-    plt.xticks([0, 120, 240, 360, 480])
+    plt.xticks([0, 120, 240, 360, 480], fontweight='bold')
     plt.ylim(0, 1)
-    plt.yticks([0, 0.2, 0.4, 0.6, 0.8, 1.0])
-    plt.ylabel('Intensity (A.U.)')
-    plt.title('pAKT')
+    plt.yticks([0, 0.2, 0.4, 0.6, 0.8, 1.0], fontweight='bold')
+    plt.ylabel('Intensity (A.U.)', fontweight='bold')
+    plt.title('pAKT', fontweight='bold')
 
     plt.subplot(2, 2, 2)
     plt.plot(sim.t, sim.pGSK3B[:, 0], 'darkblue')
@@ -30,10 +31,10 @@ def timecourse(sim):
     plt.plot(sim.t, sim.pGSK3B[:, 3], 'goldenrod')
     plt.plot(sim.t, sim.pGSK3B[:, 4], 'brown')
     plt.xlim(0, 480)
-    plt.xticks([0, 120, 240, 360, 480])
+    plt.xticks([0, 120, 240, 360, 480], fontweight='bold')
     plt.ylim(0, 1)
-    plt.yticks([0, 0.2, 0.4, 0.6, 0.8, 1.0])
-    plt.title('pGSK3β')
+    plt.yticks([0, 0.2, 0.4, 0.6, 0.8, 1.0], fontweight='bold')
+    plt.title('pGSK3β', fontweight='bold')
     
     plt.subplot(2, 2, 3)
     plt.plot(sim.t, sim.pS6K[:, 0], 'darkblue')
@@ -42,12 +43,12 @@ def timecourse(sim):
     plt.plot(sim.t, sim.pS6K[:, 3], 'goldenrod')
     plt.plot(sim.t, sim.pS6K[:, 4], 'brown')
     plt.xlim(0, 480)
-    plt.xticks([0, 120, 240, 360, 480])
+    plt.xticks([0, 120, 240, 360, 480], fontweight='bold')
     #plt.ylim(0, 1)
-    plt.yticks([0, 0.2, 0.4, 0.6, 0.8, 1.0])
-    plt.xlabel('                           time (min)')
-    plt.ylabel('Intensity (A.U.)')
-    plt.title('pS6K')
+    plt.yticks([0, 0.2, 0.4, 0.6, 0.8, 1.0], fontweight='bold')
+    plt.xlabel('                           time (min)', fontweight='bold')
+    plt.ylabel('Intensity (A.U.)', fontweight='bold')
+    plt.title('pS6K', fontweight='bold')
     
     plt.subplot(2, 2, 4)
     plt.plot(sim.t, sim.G6Pase[:, 0], 'darkblue')
@@ -56,11 +57,11 @@ def timecourse(sim):
     plt.plot(sim.t, sim.G6Pase[:, 3], 'goldenrod')
     plt.plot(sim.t, sim.G6Pase[:, 4], 'brown')
     plt.xlim(0, 480)
-    plt.xticks([0, 120, 240, 360, 480])
+    plt.xticks([0, 120, 240, 360, 480], fontweight='bold')
     plt.ylim(0, 1.2)
-    plt.yticks([0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2])
-    plt.xlabel('                           time (min)')
-    plt.title('G6Pase')
+    plt.yticks([0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2], fontweight='bold')
+    plt.xlabel('                           time (min)', fontweight='bold')
+    plt.title('G6Pase', fontweight='bold')
 
     
 
